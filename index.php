7a0f4cad483c9ebee85c724f81197d09c12723f7
Nizar Fazari <nizarfa1811@gmail.com>
2026-04-13 11:51:24 +0800

    php boss

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $name = "Zary";
+    $age = 21;
+    $city = "Mataram";
+    $status = true;
+    if ($status == true) {
+        $status = "Lajang";
+    } else {
+        $status = "Menikah";
+    }
+    echo "Nama saya $name, umur saya $age tahun dan saya tinggal di $city. Status saya $status.";
+?>
