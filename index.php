1525a616a119ec92d868786f8889720b67fe5c74
Nizar Fazari <nizarfa1811@gmail.com>
2026-04-16 08:16:28 +0800

    Belajar part 2

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     $name = "Zary";
     $age = 20;
     $city = "Mataram";
-    $status = true;
+    $status = false;
+
     if ($status == true) {
         $status = "Jomblo";
     } else {
@@ -12,4 +13,60 @@
     for ($i = 0; $i < 10; $i++) {
         echo $i;
     }
+
+    echo "<br>";
+    // OPERATOR 
+    $nilai1 = 5;
+    $nilai2 = 10;
+    $nilai3 = 10;
+    $nilai4 = 20;
+    $nilai5 = 13;
+
+    $hasil = $nilai1 + $nilai2 - $nilai3 + $nilai4 / $nilai5;
+    echo $hasil;
+
+    //percabangan
+    $nilai = 80;
+    if ($nilai >= 80) {
+        echo "A";
+    } else if ($nilai >= 70) {
+        echo "B";
+    } else if ($nilai >= 60) {
+        echo "C";
+    } else {
+        echo "D";
+    }
+
+    //perulangan
+    for ($i = 0; $i < 10; $i++) {
+        echo $i;
+    }
+
+    //array
+    $buah = array("Apel", "Mangga", "Jeruk");
+    echo $buah[0];
+    echo $buah[1];
+    echo $buah[2];
+
+    //function
+    function tambah($a, $b) {
+        return $a + $b;
+    }
+    echo tambah(10, 20);
+
+    //object
+    class Manusia {
+        public $nama;
+        public $umur;
+        public $kota;
+        public function __construct($nama, $umur, $kota) {
+            $this->nama = $nama;
+            $this->umur = $umur;
+            $this->kota = $kota;
+        }
+    }
+    $manusia = new Manusia("Zary", 20, "Mataram");
+    echo $manusia->nama;
+    echo $manusia->umur;
+    echo $manusia->kota;
 ?>
