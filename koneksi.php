4c8b7793eb223ce043033d2dcd482676f36fe866
Nizar Fazari <nizarfa1811@gmail.com>
2026-05-04 11:04:11 +0800

    database

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_login";
+
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+else{
+    echo "Koneksi database berhasil";
+}
+
+?>
